54b1ef73262c8caf61e7829a3f788713a91e4c7d
Helder Pereira <helcorintho@gmail.com>
2018-10-04 19:26:10 -0300

    Update install.sh

--- content/Postfix/make/install.sh
+++ content/Postfix/make/install.sh
@@ -14,7 +14,6 @@ apt install bind9 postfix procmail bsd-mailx telnet rsync -y
 
 configureservices () {
 clear && echo "Sincronizando arquivos de DNS e do Postfix gerados via template" && sleep 2
-test -d $FILESPATH || echo "Error, please run ./prepare before start installation" && sleep 2 && exit
 rsync -av $FILESPATH/dns/ /etc/bind/
 rsync -av $FILESPATH/postfix/ /etc/postfix/
 sleep 1
@@ -23,7 +22,7 @@ sleep 1
 configuredns () {
 clear && echo "Apontando o endereço 127.0.0.1 no arquivo resolv.conf" && sleep 2
 echo "nameserver 127.0.0.1" >  /etc/resolv.conf
-echo "search fiap.edu.br"   >  /etc/resolv.conf
+echo "search fiap.edu.br"   >> /etc/resolv.conf
 echo "nameserver 127.0.0.1" >> /etc/resolvconf/resolv.conf.d/head
 echo "search fiap.edu.br"   >> /etc/resolvconf/resolv.conf.d/head
 }
